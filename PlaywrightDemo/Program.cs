bd37dc490fe543d433649f03eccfda3a00e34233
SehbanAsif98 <sehbanasif@truemedit.com>
2026-07-15 11:50:23 +0000

    Initial Playwright C# project

---
+++ PlaywrightDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+
+await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+{
+    Headless = true
+});
+
+var page = await browser.NewPageAsync();
+
+await page.GotoAsync("https://playwright.dev");
+
+Console.WriteLine(await page.TitleAsync());
+
+await browser.CloseAsync();
